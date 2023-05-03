f872f741c9e5b830011e42cb1dcdbfad3a357f8b
Aleohl377 <aleohl@kth.se>
2023-05-03 17:29:28 +0200

    unit testing done

--- sem/src/test/RegisterTest.java
+++ sem/src/test/RegisterTest.java
@@ -34,7 +34,25 @@ public class RegisterTest {
     @Test
     public void testUpdateTotal()
     {
-        
+        ItemDTO item1 = new ItemDTO(1, "Tomat",50 , "Röd", 1, 25);
+        ItemDTO item2 = new ItemDTO(1, "Potatis",100 , "Röd", 1, 25);      
+        register.updateTotal(item2);
+        register.updateTotal(item1);
+        double TotalPrice = item1.getPrice() + item2.getPrice();
+
+        assertEquals(TotalPrice, register.getTotalPrice(), "false");
+    }
+
+    @Test
+    public void testPayment()
+    {
+        ItemDTO item1 = new ItemDTO(1, "Tomat",50 , "Röd", 1, 25);
+        ItemDTO item2 = new ItemDTO(1, "Potatis",100 , "Röd", 1, 25);      
+        register.updateTotal(item2);
+        register.updateTotal(item1);
+        double change = 200 - register.getTotalPrice();
+
+        assertEquals(change, register.payment(200), "false");
     }
 
 }
